c8f231b5a13d0e966e30fc502a8cb6ff85611c65
Takuma-Hiraoka <t-hiraoka@jsk.imi.i.u-tokyo.ac.jp>
2026-03-09 16:06:33 +0900

    can execute

--- osqp-cpp_vendor-extras.cmake
+++ osqp-cpp_vendor-extras.cmake
@@ -1,5 +1,13 @@
 find_package(Eigen3 REQUIRED)
 
+add_library(osqp-cpp SHARED IMPORTED)
+set_target_properties(osqp-cpp PROPERTIES
+  IMPORTED_LOCATION
+    "${osqp-cpp_vendor_DIR}/../../../lib/libosqp-cpp.a"
+  INTERFACE_INCLUDE_DIRECTORIES
+    "${osqp-cpp_vendor_DIR}/../../../include"
+)
+
 add_library(osqp SHARED IMPORTED)
 set_target_properties(osqp PROPERTIES
   IMPORTED_LOCATION
@@ -8,18 +16,7 @@ set_target_properties(osqp PROPERTIES
     "${osqp-cpp_vendor_DIR}/../../../include"
 )
 
-add_library(qdldl SHARED IMPORTED)
-set_target_properties(qdldl PROPERTIES
-  IMPORTED_LOCATION
-    "${osqp-cpp_vendor_DIR}/../../../lib/libqdldl.so"
-  INTERFACE_INCLUDE_DIRECTORIES
-    "${osqp-cpp_vendor_DIR}/../../../include"
-)
-
-add_library(osqp-cpp INTERFACE IMPORTED)
 target_link_libraries(osqp-cpp
   INTERFACE
-  osqp
-  qdldl
   Eigen3::Eigen
 )
